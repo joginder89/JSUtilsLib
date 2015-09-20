e63d9dc782e6e45ff3293eadbf6249ca13acd365
joginder89 <sharma.joginder89@gmail.com>
2015-09-20 13:37:03 +0530

    update Base Activity

--- jsutilsframework/src/main/java/in/jogindersharma/jsutilsframework/JSBaseActivity.java
+++ jsutilsframework/src/main/java/in/jogindersharma/jsutilsframework/JSBaseActivity.java
@@ -1,10 +1,15 @@
 package in.jogindersharma.jsutilsframework;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public abstract class JSBaseActivity extends AppCompatActivity {
 
+    ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,4 +37,50 @@ public abstract class JSBaseActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }*/
+
+    public void showProgressDialog(String message) {
+        hideProgressDialog();
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(JSBaseActivity.this);
+            mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            mProgressDialog.setCancelable(false);
+            if(message != null) {
+                mProgressDialog.setMessage(message);
+            } else {
+                mProgressDialog.setMessage("Loading...");
+            }
+            mProgressDialog.show();
+        }
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public void showAlertDialog(String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Title");
+        alertDialogBuilder.setIcon(R.drawable.notification_template_icon_bg);
+        alertDialogBuilder.setMessage(message);
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                arg0.cancel();
+            }
+        });
+
+        /*alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });*/
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 }
